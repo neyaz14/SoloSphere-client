a25b2595c27f37698d2e40edd23902431f5025ec
Neyaz Morshid <niyazmorshid@gmail.com>
2024-12-28 20:26:26 +0600

    job request page--> bid rquest done

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -6,7 +6,7 @@ const JobCard = ({job}) => {
   const {title, deadline, category, min_price, max_price, bid_count,description, _id } =job || {};
   return (
     <Link
-      // to={`/job/1`}
+      to={`/job/${_id}`}
       className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'
     >
       <div className='flex items-center justify-between'>
